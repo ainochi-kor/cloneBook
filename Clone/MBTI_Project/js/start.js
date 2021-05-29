84aa52b7f875c5fe5b9f41a87a943f7ce147a738
Tommy Kang <kthrkdals@gmail.com>
2021-05-29 19:52:10 +0900

    add MBTI_Project url 직접 구현

--- Clone/MBTI_Project/js/start.js
+++ Clone/MBTI_Project/js/start.js
@@ -132,6 +132,7 @@ function calResult() {
 
 function setResult() {
   let point = calResult();
+  sessionStorage.setItem('point', point);
   const resultName = document.querySelector('.resultName');
   resultName.innerHTML = infoList[point].name;
 
@@ -146,4 +147,16 @@ function setResult() {
   
   const resultDesc = document.querySelector('.resultDesc');
   resultDesc.innerHTML = infoList[point].desc;
+}
+
+function CopyUrlToClipboard()
+{
+  let point = sessionStorage.getItem('point');
+  
+	var obShareUrl = document.getElementById("ShareUrl");
+	obShareUrl.value = infoList[point].url;  // 현재 URL 을 세팅해 줍니다.
+	obShareUrl.select();  // 해당 값이 선택되도록 select() 합니다
+	document.execCommand("copy"); // 클립보드에 복사합니다.
+	obShareUrl.blur(); // 선택된 것을 다시 선택안된것으로 바꿈니다.
+	alert("URL이 클립보드에 복사되었습니다"); 
 }
